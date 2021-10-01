aa573b24b0169bc10d33525a4658566d34b3cd83
Laqi17 <89798279+Laqi17@users.noreply.github.com>
2021-10-01 20:44:14 +0300

    Update and rename Level.java to LevelLoaderTest.java

--- LevelLoaderTest.java
+++ LevelLoaderTest.java
@@ -1,3 +1,23 @@
+
+public class LevelLoaderTest {
+    public static void main(String[] args) {
+        //Level loaded
+        try {
+            new LevelLoader().load(new Level(10, 20));
+        } catch (LevelTooBigException ex) {
+            System.out.println("Level too big");
+        }
+
+        //Level too big
+        try {
+            new LevelLoader().load(new Level(10000, 2000));
+        } catch (LevelTooBigException ex) {
+            System.out.println("Level too big");
+        }
+    }
+}
+
+
 class Level {
     private int width;
     private int height;
@@ -26,23 +46,7 @@ class LevelLoader {
     }
 }
 
-public class LevelLoaderTest {
-    public static void main(String[] args) {
-        //Level loaded
-        try {
-            new LevelLoader().load(new Level(10, 20));
-        } catch (LevelTooBigException ex) {
-            System.out.println("Level too big");
-        }
 
-        //Level too big
-        try {
-            new LevelLoader().load(new Level(10000, 2000));
-        } catch (LevelTooBigException ex) {
-            System.out.println("Level too big");
-        }
-    }
-}
 
 class LevelTooBigException extends Exception {
     LevelTooBigException(){}
